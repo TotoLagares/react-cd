d21576127942c8b83b0886b41315d5424cc55f54
Santiago Lagares <santiagolagares2005@gmail.com>
2023-03-29 13:30:24 -0300

    entrega-1

---
+++ src/componentes/items/ItemListContainer.js
@@ -0,0 +1,8 @@
+const Greed = ({greeting}) => {
+    return (
+        <div>
+            <h1 className=" h1Greed">{greeting}</h1>
+        </div>
+    )
+}
+export default Greed
---
+++ src/componentes/navbar.js
@@ -0,0 +1,29 @@
+import Carrito from "./items/carrito"
+const Navbare = () => {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <div className="container-fluid ">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav" >
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="/"><button type="button" class="btn btn-outline-info">Info</button></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/"><button type="button" class="btn btn-outline-info">Info</button></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/"><button type="button" class="btn btn-outline-info">Info</button></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link bg-danger bg-opacity-75 text-white" href="/">< Carrito /></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    )
+    }    
+export default Navbare
